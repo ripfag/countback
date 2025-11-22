772af8c154da6f8e31f991abe27dc7ee5e1aeffd
ripfag <aleksandrbistrov1@bk.ru>
2025-11-22 17:32:43 +0300

    Update count_back.py

--- count_back.py
+++ count_back.py
@@ -1,20 +1,20 @@
-get_number = None
-while True:
+def count_back():
     try:
-        get_number = float(input("Введите положительное число: "))
-        if get_number < 0:
-            print("Введите целое положительное число!")
-
-
-        while get_number > -1:
-            print("Счетчик: ",get_number)
-            get_number -= 1
+        get_number = int(input("Введите число: "))
+    except ValueError:
+        print("Введено не число, введите число!")
+        return count_back()
+    if get_number < 0:
+        print("Введите целое положительное число!")
+        return count_back()
+    else:
+        while True:
+            if get_number > -1:
+                print("Счетчик: ", get_number)
+                get_number -= 1
             if get_number == -1:
                 break
 
 
-    except ValueError:
-        print("Введено не число, введите число!")
-    except TypeError:
-        print("Введите целое положительное число!")
+count_back()
 
